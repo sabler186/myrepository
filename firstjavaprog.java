a68e0274b07682b072a0a741144cf7ed262922dd
sabler186 <158246417+sabler186@users.noreply.github.com>
2024-02-03 20:04:41 +0530

    Update firstjavaprog.java

--- firstjavaprog.java
+++ firstjavaprog.java
@@ -1,5 +1,7 @@
 class simple {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("What is String in Java?"); 
+         System.out.println("Generally, String is a sequence of characters. But in Java, string is an object that represents a sequence of characters. The java.lang.String class is used to create a string object.");
     }
 }
+
